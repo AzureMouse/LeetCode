224e76830b47a2183cc4201560e122603c66055c
AzureMouse <117840908+AzureMouse@users.noreply.github.com>
2022-12-05 20:40:41 -0500

    Add files via upload

---
+++ Easy/ArrayConcatenation.java
@@ -0,0 +1,21 @@
+/**
+* File:			ArrayConcatenation.java
+* Project:		LeetCode
+* Date:			12/05/22
+* Author:		AzureMouse
+* Description:
+*
+* Returns a new array with 2 times of the same array
+*
+**/
+
+public ArrayConcatenation{
+	public int[] getConcatenation(int[] nums){
+		final int LENGTH = nums.LENGTH;
+		int[] numsClone = new int[LENGTH * 2];
+		System.arraycopy(nums, 0, numsClone, 0, LENGTH); // -> Copy array
+		System.arraycopy(nums, 0, numsClone, LENGTH, LENGTH); // -> Copy array starting at 0 to new array at the position of length with LENGTH numbers copied
+
+		return numsClone;
+	}
+}
