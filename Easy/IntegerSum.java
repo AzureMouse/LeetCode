9924fc8132cfc2513ec9017bacf981bf83e0ada2
AzureMouse <117840908+AzureMouse@users.noreply.github.com>
2022-12-06 08:20:19 -0500

    Add files via upload

---
+++ Easy/IntegerSum.java
@@ -0,0 +1,15 @@
+/**
+ * File:		IntegerSum.java
+ * Project:		LeetCode
+ * Date:		12/06/22
+ * Author:		AzureMouse
+ * Description:
+ *
+ * Adds two integers together and returns the result
+**/
+
+public class IntegerSum{
+	public int sum(int num1, int num2){
+		return (num1 + num2);
+	}
+}
